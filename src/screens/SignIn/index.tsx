96e0ca9e5fea212a86d68a7ee01fa66ab834cb90
Carlos Henrique <caique.h@hotmail.com>
2021-09-09 20:18:46 -0300

    Added react-navitagion

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,16 +1,25 @@
-import React from "react";
-import { Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import { Image, Text, TextInput, View } from "react-native";
 import { styles } from './style'
 
 export function SignIn(){
+
+	const [ name, setName ] = useState('');
+
 	return (
 		<View style={styles.container}>
+
 				<Text style={styles.text}>
 					<Text style={{ fontWeight: 'bold' }}>Bem Vindo!</Text>{"\n"}
 					Qual o seu nome?
 				</Text>
 
-				<TextInput style={styles.input}></TextInput>
+				<TextInput 
+					style={styles.input} 
+					onChangeText={(value) => setName(value)}
+					/>
+
+				<Text style={styles.text}>{ name }</Text>
 				
 		</View>
 	)
